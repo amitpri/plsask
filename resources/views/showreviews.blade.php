3d67ff8f669405af7e7493c51405559f59865812
Amit Priydarshi <=>
2017-09-03 09:59:30 +0530

    googleApi Title Logo

--- resources/views/showreviews.blade.php
+++ resources/views/showreviews.blade.php
@@ -8,7 +8,7 @@
  
 		<meta charset="UTF-8">
 
-		<title>ASKPLS - MR Management Portal</title>
+		<title>PlsAsk - Reviews</title>
 		<meta name="keywords" content="HTML5 Admin Template" />
 		<meta name="description" content="Porto Admin - Responsive HTML5 Template">
 		<meta name="author" content="okler.net">
@@ -17,7 +17,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
 		<!-- Web Fonts  -->
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
+		<link href="css/googlefonts_shadow_intro_light.css" rel="stylesheet" type="text/css">
 
 		<!-- Vendor CSS -->
 		<script src="/admin/assets/vendor/jquery/jquery.js"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Please Ask - Private Anonymous Review Platform</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="css/googlefonts_shadow_intro_light.css" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
