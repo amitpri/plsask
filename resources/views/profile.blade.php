ca7a877a5556b44cf7e58b3e16cae260f4395841
Amit Priydarshi <=>
2017-09-07 22:13:39 +0530

    password

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -79,9 +79,6 @@
 				<li>
 					<a href="#company" data-toggle="tab">Company</a>
 				</li>
-				<li>
-					<a href="#password" data-toggle="tab">Password</a>
-				</li>
 			</ul>
 			<div class="tab-content">
 				Hi there {{ session('data') ?? '' }}
@@ -251,67 +248,6 @@
 						{!! Form::close() !!}
 					</form>
 						 
-
-				</div>
-				<div id="password" class="tab-pane">
-
-					<form action="change-password" method="post" role="form" class="form-horizontal">
-
-						{{csrf_field()}}
-						 
-						<h4 class="mb-xlg">Change Password</h4>
-					 
-						<div class="form-group{{ $errors->has('old') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Old Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="old">
-
-                                @if ($errors->has('old'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('old') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                    <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                </span>
-                                @endif
-                            </div>
-                        </div>
-					 
-						<div class="panel-footer">
-							<div class="row">
-								<div class="col-md-12 col-md-offset-3">
-									<button type="submit" class="btn btn-primary">Submit</button> 
-								</div>
-							</div>
-						</div>
-
-					</form>
 
 				</div>
 			</div>
